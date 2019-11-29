b30bb497e43e7d6f1ff65d2760b8d59bf90f1c44
VectorLineal <jnobzaa@unal.edu.co>
2019-11-28 23:13:21 -0500

    se arregla problema de la camara y se pone al primer personaje, se migra a phaser 3.3

--- sand/js/character/Character.js
+++ sand/js/character/Character.js
@@ -1,8 +1,9 @@
 const Atribute = require("./Atribute");
 
 class Character{
-    constructor(name, type, race, baseStr, strGrowth, baseRes, resGrowth, baseAgi, agiGrowth, basePer, perGrowth, baseInt, intGrowth, baseDet, detGrowth, level){
+    constructor(name, type, race, baseStr, strGrowth, baseRes, resGrowth, baseAgi, agiGrowth, basePer, perGrowth, baseInt, intGrowth, baseDet, detGrowth, level, body, lArm, rArm){
         this.name = name;
+        this.level = level;
         this.type = type;
         this.race = race;
         this.str = new Atribute("stregth", baseStr, strGrowth);
@@ -11,6 +12,13 @@ class Character{
         this.per = new Atribute("stregth", basePer, perGrowth);
         this.int = new Atribute("stregth", baseInt, intGrowth);
         this.det = new Atribute("stregth", baseDet, detGrowth);
+        this.str.update(level);
+        this.res.update(level);
+        this.agi.update(level);
+        this.per.update(level);
+        this.int.update(level);
+        this.det.update(level);
+        
         this.skills = [];
         this.bonusStats = [];
         this.debuffs = [];
